c0db0215d45669978ac09e458fc24987e934d4ae
Yuen Mei Wan <wanyuenmei@gmail.com>
2015-05-07 02:20:32 -0700

    first commit

--- button/ViewController.swift
+++ button/ViewController.swift
@@ -9,16 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var scoreLabel: UILabel!
+//    @IBOutlet var button: UIButton!
+    
+    
+//    var globalScore = 0
+    var local = 0
+
+    
+    
+    @IBAction func buttonPressed() {
+        local++
+        NSLog("\(local)")
+        scoreLabel.text = "Score: \(local)"
+//        button
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "nyan_cat_wallpaper_by_jeremifier-d3kf4fj.png")!)
+        
+        scoreLabel.backgroundColor = UIColor.whiteColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
